c88c8be7e48094369bd6c0bb085c538224da2242
Kirill Goltsman <goltsmank@gmail.com>
2024-10-06 14:24:53 +0200

    good

--- shape_transfer.py
+++ shape_transfer.py
@@ -152,3 +152,19 @@ if __name__ == "__main__":
     ax.scatter(warped_vert[:, 0], warped_vert[:, 1], warped_vert[:, 2])
     plt.show()
     print("done")
+
+    import matplotlib.pyplot as plt
+
+    warped_vert = np.random.rand(100, 3)  # A sample 3D point cloud
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(warped_vert[:, 0], warped_vert[:, 1], warped_vert[:, 2])
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+    print("done")
